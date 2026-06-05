5062d37572cb8d749fe7798871f5157287625ff1
Boualleg Kaissar <kkoussay113@gmail.com>
2026-06-05 19:29:21 +0100

    Simple Player Movement

--- source/scripts/Player/player.gd
+++ source/scripts/Player/player.gd
@@ -1,1 +1,25 @@
 extends CharacterBody2D
+
+var speed := 400.0
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	movement_handle(delta)
+	gravity_handle(delta)
+	move_and_slide()
+	print("Velocity: ", velocity)
+
+func gravity_handle(delta: float):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	else :
+		velocity.y = 0
+
+func movement_handle(delta: float):
+	var joystick := Input.get_vector("Left", "Right", "Up", "Down")
+	if joystick.x > 0:
+		velocity.x = speed
+	elif joystick.x < 0:
+		velocity.x = -speed
+	else :
+		velocity.x = 0
