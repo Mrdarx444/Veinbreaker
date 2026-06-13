a011e971ac1b85d1013212cb8c480269ae5a5a58
Boualleg Kaissar <kkoussay113@gmail.com>
2026-06-13 21:14:53 +0100

    Joystick and Aiming Simple Controllers

---
+++ source/scripts/Player/joystick.gd
@@ -0,0 +1,50 @@
+class_name PlayerJoystick
+extends VirtualJoystick
+
+enum AimZone { MOVE, MOVE_AIM_UP, MOVE_AIM_DOWN, AIM_UP_IDLE }
+enum AimDirection { FORWARD, UP, DOWN }
+
+# Debugg
+var aim_zone_debbug := {
+	AimZone.MOVE: "Move",
+	AimZone.MOVE_AIM_UP: "Move Aim Up",
+	AimZone.MOVE_AIM_DOWN: "Move Aim Down",
+	AimZone.AIM_UP_IDLE: "Aim Up Idle"
+}
+
+var aim_direction_debbug := {
+	AimDirection.FORWARD: "Forward",
+	AimDirection.UP: "Up",
+	AimDirection.DOWN: "Down"
+}
+
+@export_range(0, 1, 0.01) var AIM_THRESHOLD  : float = 0.5
+@export_range(0, 1, 0.01) var IDLE_THRESHOLD : float = 0.2
+
+var current_zone     : AimZone     = AimZone.MOVE
+var aim_direction    : AimDirection = AimDirection.FORWARD
+var move_direction   : float = 0.0  # -1, 0, 1
+
+func _process(delta: float) -> void:
+	var input_vector := Input.get_vector("Left", "Right", "Up", "Down")
+	var x = input_vector.x
+	var y = input_vector.y
+	move_direction = 0.0
+
+	if abs(x) > IDLE_THRESHOLD:
+		move_direction = sign(x)
+		if y < -AIM_THRESHOLD:
+			current_zone  = AimZone.MOVE_AIM_UP
+			aim_direction = AimDirection.UP
+		elif y > AIM_THRESHOLD:
+			current_zone  = AimZone.MOVE_AIM_DOWN
+			aim_direction = AimDirection.DOWN
+		else:
+			current_zone  = AimZone.MOVE
+			aim_direction = AimDirection.FORWARD
+	elif y < -AIM_THRESHOLD:
+		current_zone  = AimZone.AIM_UP_IDLE
+		aim_direction = AimDirection.UP
+	else:
+		current_zone  = AimZone.MOVE
+		aim_direction = AimDirection.FORWARD
--- source/scripts/Player/player.gd
+++ source/scripts/Player/player.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
-var speed := 400.0
-var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export_subgroup("Movement")
+@export var speed : float = 700.0
+@export_range(0, 1, 0.01) var aiming_slowdown_ratio: float = 0.75
+@export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var joystick: PlayerJoystick = $HUD/PlayerJoystick
+
+# Debugging
+@onready var zone_label: Label = $HUD/Zone
+@onready var direction_label: Label = $HUD/Direction
+@onready var move_direction_label: Label = $HUD/move_direction
+@onready var velocity_label: Label = $HUD/Velocity
+
 
 func _physics_process(delta: float) -> void:
 	movement_handle(delta)
 	gravity_handle(delta)
 	move_and_slide()
-	print("Velocity: ", velocity)
+	_debugg()
 
 func gravity_handle(delta: float):
 	if !is_on_floor():
@@ -16,10 +27,24 @@ func gravity_handle(delta: float):
 		velocity.y = 0
 
 func movement_handle(delta: float):
-	var joystick := Input.get_vector("Left", "Right", "Up", "Down")
-	if joystick.x > 0:
-		velocity.x = speed
-	elif joystick.x < 0:
-		velocity.x = -speed
-	else :
-		velocity.x = 0
+	match joystick.current_zone:
+		joystick.AimZone.MOVE:
+			velocity.x = speed * joystick.move_direction
+		joystick.AimZone.MOVE_AIM_UP, joystick.AimZone.MOVE_AIM_UP:
+			velocity.x = speed * joystick.move_direction * aiming_slowdown_ratio
+		_:
+			velocity.x = 0
+
+func _debugg():
+	zone_label.text = "Aim Zone: " + joystick.aim_zone_debbug[joystick.current_zone]
+	direction_label.text = "Aim Direction: " + joystick.aim_direction_debbug[joystick.aim_direction]
+	
+	match joystick.move_direction:
+		-1:
+			move_direction_label.text = "Direction: Left"
+		1:
+			move_direction_label.text = "Direction: Right"
+		_:
+			move_direction_label.text = "Direction: None"
+	
+	velocity_label.text = "Velocity: " + str(velocity)
