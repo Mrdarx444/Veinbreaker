0c7cf45042f7e8c9734fd6547940835f2b38a8a2
Boualleg Kaissar <kkoussay113@gmail.com>
2026-06-13 22:57:25 +0100

    Add acceleration/friction

--- source/scripts/Player/player.gd
+++ source/scripts/Player/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 @export_subgroup("Movement")
-@export var speed : float = 700.0
+@export var speed: float = 600.0
+@export var acceleration: float = speed * 8
+@export var friction: float = speed * 10
 @export_range(0, 1, 0.01) var aiming_slowdown_ratio: float = 0.75
 @export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var max_fall_speed: float = 1500.0
 
 @onready var joystick: PlayerAimComponent = $Components/PlayerAimComponent
 
@@ -21,19 +24,31 @@ func _physics_process(delta: float) -> void:
 	_debugg()
 
 func gravity_handle(delta: float):
-	if !is_on_floor():
-		velocity.y += gravity * delta
+	if !is_on_floor() and velocity.y < max_fall_speed:
+		velocity.y = min(velocity.y + gravity * delta, max_fall_speed)
 	else :
 		velocity.y = 0
 
 func movement_handle(delta: float):
 	match joystick.current_zone:
 		joystick.AimZone.MOVE:
-			velocity.x = speed * joystick.move_direction
-		joystick.AimZone.MOVE_AIM_UP, joystick.AimZone.MOVE_AIM_UP:
-			velocity.x = speed * joystick.move_direction * aiming_slowdown_ratio
+			velocity.x = move_toward(
+				velocity.x,
+				speed * joystick.move_direction,
+				acceleration * delta
+			)
+		joystick.AimZone.MOVE_AIM_UP, joystick.AimZone.MOVE_AIM_DOWN:
+			velocity.x = move_toward(
+				velocity.x,
+				speed * joystick.move_direction * aiming_slowdown_ratio,
+				acceleration * delta
+			)
 		_:
-			velocity.x = 0
+			velocity.x = move_toward(
+				velocity.x,
+				0.0,
+				friction * delta
+			)
 
 func _debugg():
 	zone_label.text = "Aim Zone: " + joystick.aim_zone_debbug[joystick.current_zone]
